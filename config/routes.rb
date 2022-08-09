cb3ab82f2e38eee59424a58ac42822fb91510183
aboongm <aboongm@yahoo.com>
2022-08-09 20:29:18 +0530

    Update the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "splashes#index"
   devise_for :users
+
+  resources :categories
+  resources :expenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
