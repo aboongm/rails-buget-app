55d4a817ca3dbc805a3af5715d027ba2d1807b82
aboongm <aboongm@yahoo.com>
2022-08-09 20:03:22 +0530

    Create the login and sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+  root "splashes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
