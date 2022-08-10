f1006da044edf7435e5c1e5e059d896e473010d3
aboongm <aboongm@yahoo.com>
2022-08-10 22:30:12 +0530

    Add rspec in gemfile and create test for users

---
+++ spec/models/users_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    before(:all) do
+        @user = User.new(name: "Ranjit", email: "example@test.com", password: "123456")
+    end
+
+    describe "Validations" do
+        it "should be valid" do
+            expect(@user).to be_valid
+        end
+
+        it "should not be valid without name" do
+            @user.name = nil
+            expect(@user).to_not be_valid
+        end
+
+        it "should not be valid without email" do
+            @user.email = nil
+            expect(@user).to_not be_valid
+        end
+
+        it "should not be valid without password" do
+            @user.password = nil
+            expect(@user).to_not be_valid
+        end
+    end
+end
