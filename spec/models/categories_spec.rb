2589897999f57980baa66883ca25f9939bfe3d66
aboongm <aboongm@yahoo.com>
2022-08-10 23:03:55 +0530

    Corrections for linter

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-    before(:all) do 
-        @user = User.create(name: "Ran", email: "example2@test.com", password: "123456")
-        expect(@user).to be_valid
-        @category = Category.new(name: "Fast-food", icon: "fast-food.png", user_id: @user.id)
-    end
+  before(:all) do
+    @user = User.create(name: 'Ran', email: 'example2@test.com', password: '123456')
+    expect(@user).to be_valid
+    @category = Category.new(name: 'Fast-food', icon: 'fast-food.png', user_id: @user.id)
+  end
 
-    describe "Validations" do
-        it "should be valid" do
-            expect(@category).to be_valid
-            expect(@category.name).to eq("Fast-food")
-            expect(@category.icon).to eq("fast-food.png")
-        end
+  describe 'Validations' do
+    it 'should be valid' do
+      expect(@category).to be_valid
+      expect(@category.name).to eq('Fast-food')
+      expect(@category.icon).to eq('fast-food.png')
     end
+  end
 end
