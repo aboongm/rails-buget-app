95089c7b06f35d609d6e5630ea99457bc7484bfd
aboongm <aboongm@yahoo.com>
2022-08-10 22:52:04 +0530

    Create test for categories

---
+++ spec/models/categories_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Category, type: :model do
+    before(:all) do 
+        @user = User.create(name: "Ran", email: "ran2@test.com", password: "123456")
+        expect(@user).to be_valid
+        @category = Category.new(name: "Fast-food", icon: "fast-food.png", user_id: @user.id)
+    end
+
+    describe "Validations" do
+        it "should be valid" do
+            expect(@category).to be_valid
+            expect(@category.name).to eq("Fast-food")
+            expect(@category.icon).to eq("fast-food.png")
+        end
+    end
+end
