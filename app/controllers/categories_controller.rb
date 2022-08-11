5357a9c74f7d6b72cdc1fd1c3d7832b004e187cd
aboongm <aboongm@yahoo.com>
2022-08-11 20:18:05 +0530

    Update style and create code for form to add new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,11 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :set_category, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:public]
 
   def index
     @categories = Category.where(user_id: current_user.id)
   end
-  
+
   def show
     @expenses = Expense.all.where(category_id: @category.id)
   end
@@ -19,29 +19,22 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     @category.user_id = current_user.id
-    
+
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_path(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
       else
-        format.html {render :new, status: unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
       end
-    end
-  end
-
-  def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_path, notice: 'Category was successfully destroyed.' }
     end
   end
 
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_path(@category), notice: 'Category was successfully updated.' }
+        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
       else
-        format.html { render :edit, status: unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end
---
+++ app/helpers/categories_helper.rb
@@ -0,0 +1,6 @@
+module CategoriesHelper
+    def total_amount_helper(expenses)
+      expenses.reduce(0) { |sum, expense| sum + expense.amount }
+    end
+  end
+  
