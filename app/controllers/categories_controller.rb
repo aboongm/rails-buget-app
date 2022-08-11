bb72a42cef2aeec9cdfae3c6ec468ae66380bb11
aboongm <aboongm@yahoo.com>
2022-08-11 08:30:15 +0530

    Generate categories and expenses views along with spec files

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,40 +1,19 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: %i[index show]
-
   def index
-    @categories = Category.where(user_id: current_user.id)
-  end
-
-  def show
-    @category = Expense.all.where(category_id: @category.id)
   end
 
   def new
-    @category = Category.new
   end
 
-  def edit; end
+  def create
+  end
 
-  def create; end
+  def show
+  end
+
+  def destroy
+  end
 
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category(@category), notice: 'Category was successfully updated.' }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  private
-
-  def set_category
-    @category = Category.find(params[:id])
-  end
-
-  def category_params
-    params.require(:category).permit(:name, :icon)
   end
 end
---
+++ spec/requests/expenses_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Expenses", type: :request do
+  describe "GET /new" do
+    it "returns http success" do
+      get "/expenses/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/expenses/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/expenses/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/expenses/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/expenses/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
